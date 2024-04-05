456d362dbf12bd014e6a77335c9848db2fe3c220
dinhhuy <tdhuy1410@gmail.com>
2024-04-05 13:11:55 +0900

    fix link

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,35 +1,35 @@
-import EditTicketForm from "@/app/(components)/EditTicketForm";
+import EditTicketForm from '@/app/(components)/EditTicketForm';
 
 const getTicketById = async (id) => {
-  try {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-      cache: "no-store",
-    });
+    try {
+        const res = await fetch(`https://ticketjiraapp.netlify.app//api/Tickets/${id}`, {
+            cache: 'no-store',
+        });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch topic");
+        if (!res.ok) {
+            throw new Error('Failed to fetch topic');
+        }
+
+        return res.json();
+    } catch (error) {
+        console.log(error);
     }
-
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 let updateTicketData = {};
 const TicketPage = async ({ params }) => {
-  const EDITMODE = params.id === "new" ? false : true;
+    const EDITMODE = params.id === 'new' ? false : true;
 
-  if (EDITMODE) {
-    updateTicketData = await getTicketById(params.id);
-    updateTicketData = updateTicketData.foundTicket;
-  } else {
-    updateTicketData = {
-      _id: "new",
-    };
-  }
+    if (EDITMODE) {
+        updateTicketData = await getTicketById(params.id);
+        updateTicketData = updateTicketData.foundTicket;
+    } else {
+        updateTicketData = {
+            _id: 'new',
+        };
+    }
 
-  return <EditTicketForm ticket={updateTicketData} />;
+    return <EditTicketForm ticket={updateTicketData} />;
 };
 
 export default TicketPage;
